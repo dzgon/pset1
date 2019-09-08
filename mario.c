fcaaa84a56fac4ab0a8419d262a75a437d66486a
dzgon <dominicmerchelski@gmail.com>
2019-09-08 15:07:37 -0400

    Polishing mario.c
    title

--- mario.c
+++ mario.c
@@ -6,43 +6,42 @@
 int main (void)
 {
     int mario_height;
-
+    // We use a "do while loop" here instead of a for loop because we need to prompt
+    // at least once, and we dont know how many times we will need to loop
     do
     {
-        // Prompting the user for a value for height
+        // Prompting the user for an integer to represent the pyramid height
         mario_height = get_int("Height: ");
     }
-    // Repeatedly asking as long as value inputed is negative or greater than 8
+    // Repeatedly prompting as long as the value inputed is negative or greater than 23
     while (mario_height < 1 || mario_height > 23);
 
-    printf("%i\n", mario_height);
 
-
-    // Repeating loop for number of lines(height) that the pyramid will be
+    // Repeating loop for a number of times equal to the pyramid height
     for(int i = 0; i < mario_height; i++)
     {
-        // Creates the spaces before the pyramid blocks to line them up
+        // Creates the spaces before the pyramid bricks to line them up
         for (int j = 0; j + i < mario_height - 1; j++)
         {
             printf(" ");
         }
-        // Creates the "blocks" of the pyramid
+        // Creates the "bricks" of the pyramid for the first half
         for (int k = 0; k < 1 + i; k++)
         {
             printf("#");
         }
-        // Creates the spaces between the two half pyramids
+        // Creates the static spaces between the two halfs of the pyramid
         for (int l = 0; l < 2; l++)
         {
             printf(" ");
         }
-        // Creates the "blocks" of the pyramid for the other side
+        // Creates the "bricks" of the pyramid for the second half
         for (int k = 0; k < 1 + i; k++)
         {
             printf("#");
         }
 
-        // Goes to the next line to build next layer of blocks
+        // Is used to go to the next line after each loop
         printf("\n");
     }
 
