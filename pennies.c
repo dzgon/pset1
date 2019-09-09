015def99e5e4814696ba385b89dcc8489af32f39
dzgon <dzgongithub@gmail.com>
2019-09-08 21:19:32 -0400

    changing variable name and adding comments

--- pennies.c
+++ pennies.c
@@ -17,24 +17,27 @@ int main (void)
 
     // Using the "long long" data type because numbers can get really big
     // and run out of room in the regular "int" data type
-    long long start_pen = 0;
+    long long cumulative_pen = 0;
     do
     {
-        start_pen = get_int("Pennies on first day: ");
+        cumulative_pen = get_int("Pennies on first day: ");
     }
     // Repeatedly prompts user for # of pennies on day 1 as long as the
     // inputed value is a negative integer or 0
-    while (start_pen < 1);
+    while (cumulative_pen < 1);
+    
+    // Storing pennies we started with on day one
     long long day_one = start_pen;
+    
     // Doubles the starting pennies everyday for each day in the month
     for (int i = 0; i < days_month; i++)
     {
-        start_pen = start_pen * 2;
+        cumulative_pen = start_pen * 2;
     }
     
     // Subtract pennies pennies we started with from the total amount of pennies
-    start_pen -= day_one;
+    cumulative_pen -= day_one;
     // Change the data type into a "long double" before converting into dollars
     // and printing so that we can see decimals
-    printf("$%.2Lf\n", (long double)(start_pen) / 100);
+    printf("$%.2Lf\n", (long double)(cumulative_pen) / 100);
 }
